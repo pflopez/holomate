8ca98b4991b9a8fb7d5e32bb5298a42117017127
Pablo Lopez <pablo.lopez@splice.com>
2022-06-28 16:57:47 -0300

    added icons and order on notes

--- src/app/holomate/holomate.component.ts
+++ src/app/holomate/holomate.component.ts
@@ -11,37 +11,23 @@ export class HolomateComponent implements OnInit {
 
 
   notes = [
-    {note: 'C4', active: false},
-    {note: 'C#4', active: false},
-    {note: 'D4', active: false},
-    {note: 'D#4', active: false},
-    {note: 'E4', active: false},
-    {note: 'F4', active: false},
-    {note: 'F#4', active: false},
-    {note: 'G4', active: false},
-    {note: 'G#4', active: false},
-    {note: 'A4', active: false},
-    {note: 'A#4', active: false},
-    {note: 'B4', active: false},
+    {note: 'A4', active: false, icon: 'vientito-01.png'},
+    {note: 'A#4', active: false, icon: 'vientito-02.png'},
+    {note: 'B4', active: false, icon: 'vientito-03.png'},
+
+    {note: 'F#4', active: false, icon: 'vientito-04.png'},
+    {note: 'G4', active: false, icon: 'vientito-05.png'},
+    {note: 'G#4', active: false, icon: 'vientito-06.png'},
+
+    {note: 'D#4', active: false, icon: 'vientito-07.png'},
+    {note: 'E4', active: false, icon: 'vientito-08.png'},
+    {note: 'F4', active: false, icon: 'vientito-09.png'},
+
+    {note: 'C4', active: false, icon: 'vientito-10.png'},
+    {note: 'C#4', active: false, icon: 'vientito-11.png'},
+    {note: 'D4', active: false, icon: 'vientito-12.png'},
   ]
 
-
-  notesV1 = [
-    {note: 'B4', active: false},
-    {note: 'G#4', active: false},
-    {note: 'F4', active: false},
-    {note: 'D4', active: false},
-
-    {note: 'A#4', active: false},
-    {note: 'G4', active: false},
-    {note: 'E4', active: false},
-    {note: 'C#4', active: false},
-
-    {note: 'A4', active: false},
-    {note: 'F#4', active: false},
-    {note: 'D#4', active: false},
-    {note: 'C4', active: false},
-  ]
 
   activeNotes: string[] = [];
 
