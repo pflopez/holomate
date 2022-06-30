44cb81c64f4dc12e27f02bd7ce1198a6197917a9
Pablo Lopez <pablo.lopez@splice.com>
2022-06-30 01:14:39 -0300

    visualizer ish

--- src/app/visualizer/visualizer.component.ts
+++ src/app/visualizer/visualizer.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Howler} from "howler";
 
 @Component({
   selector: 'app-visualizer',
@@ -7,9 +8,74 @@ import {Component, OnInit} from '@angular/core';
 })
 export class VisualizerComponent implements OnInit {
 
-  constructor() { }
+  analyser: any;
+  ctx: any;
+  constructor() {
+  }
 
   ngOnInit(): void {
+    const WIDTH = 500;
+    const HEIGHT = 500;
+    const canvas = document.querySelector('canvas');
+    if (canvas) {
+      console.log('hay canvas')
+      this.ctx = canvas.getContext('2d');
+      canvas.width = WIDTH;
+      canvas.height = HEIGHT;
+
+      this.getAudio();
+
+    }
+
+  }
+
+
+  getAudio() {
+    // Create an analyser node in the Howler WebAudio context
+    this.analyser = Howler.ctx.createAnalyser();
+
+    // Connect the masterGain -> analyser (disconnecting masterGain -> destination)
+    Howler.masterGain.connect(this.analyser);
+
+    // Connect the analyser -> destination
+    this.analyser.connect(Howler.ctx.destination);
+    // How much data should we collect
+    this.analyser.fftSize = 2 ** 10;
+    // pull the data off the audio
+    const timeData = new Uint8Array(this.analyser.frequencyBinCount);
+    this.drawTimeData(timeData);
+  }
+
+  drawTimeData(timeData: any) {
+
+    if(this.analyser){
+      this.analyser.getByteTimeDomainData(timeData);
+    }
+    if(this.ctx){
+      this.ctx.lineWidth = 4;
+      this.ctx.strokeStyle = "#ffc600";
+      this.ctx.clearRect(0, 0, 500, 500);
+      this.ctx.beginPath();
+      const bufferLength = this.analyser.frequencyBinCount;
+      const sliceWidth = 500 / bufferLength;
+      let x = 0;
+      timeData.forEach((data: any, i: number) => {
+        const v = data / 128;
+        const y = (v * 500) / 2;
+        // draw our lines
+        if (i === 0) {
+          this.ctx.moveTo(x, y);
+        } else {
+          this.ctx.lineTo(x, y);
+        }
+        x += sliceWidth;
+      });
+      this.ctx.stroke();
+
+    }
+
+    // call itself as soon as possible
+    requestAnimationFrame(() => this.drawTimeData(timeData));
   }
 
 
